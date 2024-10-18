b88df167af23334dfd0ef1e6fad482341cc19902
Sanket Rathod <118626260+sanketrathod07@users.noreply.github.com>
2024-10-18 20:36:06 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -48,11 +48,13 @@ const Home = () => {
   useEffect(() => {
     const socketConnection = io(process.env.REACT_APP_BACKEND_URL, {
       transports: ['websocket'],
-      auth: {
-        token: localStorage.getItem('token')
-      },
-      secure: true
-    })
+      auth: { token: localStorage.getItem('token') },
+      secure: true,
+      reconnectionAttempts: 5,
+      pingInterval: 25000, // Custom ping interval
+      pingTimeout: 60000 // Timeout before considering connection lost
+    });
+
 
     socketConnection.on('onlineUser', (data) => {
       dispatch(setOnlineUser(data))
